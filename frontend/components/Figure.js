5f5da0cd4fb19b6c863bd60ccdac72bb461b80a2
walyn smithe <walynsmithe@yahooe.com>
2023-10-12 18:43:52 -0500

    updated

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,12 +1,22 @@
 import React from "react"
+import styled from 'styled-components'
+
+const StyledFigured = styled.figure`
+max-width: 60%;
+
+figcaption {
+    font-style: italic;
+}
+`
 
 export default function Figure({ imageURL, caption}) {
     return(
-     
-      <figure>
+      <StyledFigured>
         <img src ={imageURL} />
-        <figcaption>Awesome pic taken on {caption}</figcaption>
-      </figure>
+        <figcaption>
+            Awesome pic taken on {caption}
+        </figcaption>
+      </StyledFigured>
     
     )
   }
